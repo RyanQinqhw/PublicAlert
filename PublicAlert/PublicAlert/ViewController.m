5251322b18d3bb26fa6b06b72cd48879f02afb2e
明镜止水 <Xray@mingjingzhishuideMacBook-Pro.local>
2017-12-11 14:57:45 +0800

    添加uiview分类快速设置frame

--- PublicAlert/PublicAlert/ViewController.m
+++ PublicAlert/PublicAlert/ViewController.m
@@ -16,7 +16,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn1 = [UIButton new];
+    [btn1 setTitle:@"一个按钮的弹框" forState:UIControlStateNormal];
+    btn1.backgroundColor = [UIColor grayColor];
+    [btn1 sizeToFit];
+    
+    UIButton *btn2 = [UIButton new];
+    [btn2 setTitle:@"两个按钮的弹框" forState:UIControlStateNormal];
+    btn1.backgroundColor = [UIColor grayColor];
+    [btn1 sizeToFit];
+    
+    UIButton *btn3 = [UIButton new];
+    [btn3 setTitle:@"三个按钮的弹框" forState:UIControlStateNormal];
+    btn1.backgroundColor = [UIColor grayColor];
+    [btn1 sizeToFit];
+    
+    UIButton *btn4 = [UIButton new];
+    [btn4 setTitle:@"三个按钮的弹框" forState:UIControlStateNormal];
+    btn1.backgroundColor = [UIColor grayColor];
+    [btn1 sizeToFit];
+    
+    [self.view addSubview:btn1];
+    [self.view addSubview:btn2];
+    [self.view addSubview:btn3];
+    [self.view addSubview:btn4];
+    
 }
 
 
